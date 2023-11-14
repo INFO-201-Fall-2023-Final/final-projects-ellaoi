fb9694c7eb7b43e57842f7afcb66fffb8640b0f0
ellaoi <ellaoi@uw.edu>
2023-11-14 12:15:02 -0800

    FINAL PROJECT

---
+++ FINAL PROJECT/Final_Project.R
@@ -0,0 +1,8 @@
+
+
+library(dplyr)
+library(stringr)
+
+#Loading in datasets 
+overall_ob <- read.csv("2022-overall-prevalence.csv")
+youth_ob <- read.csv("Nutrition__Physical_Activity__and_Obesity_-_Youth_Risk_Behavior_Surveillance_System.csv")

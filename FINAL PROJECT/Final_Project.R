f4f7f585d6910e60892e36a0d22eb64658dd960a
ellaoi <ellaoi@uw.edu>
2023-11-14 17:37:08 -0800

    FINAL PROJECT

--- FINAL PROJECT/Final_Project.R
+++ FINAL PROJECT/Final_Project.R
@@ -6,3 +6,10 @@ library(stringr)
 #Loading in datasets 
 overall_ob <- read.csv("2022-overall-prevalence.csv")
 youth_ob <- read.csv("Nutrition__Physical_Activity__and_Obesity_-_Youth_Risk_Behavior_Surveillance_System.csv")
+
+#joining the datasets
+
+colnames(youth_ob)[colnames(youth_ob) == "LocationDesc"] <- "State"
+
+ob <- left_join(youth_ob, overall_ob, by = "State")
+
